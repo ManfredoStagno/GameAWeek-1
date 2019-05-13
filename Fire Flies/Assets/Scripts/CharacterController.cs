ad1ff326e45f54b325d7a17b22467e12fd865bc2
Manfredo Stagno d'Alcontres <manfredo@stagno.it>
2019-05-14 00:18:09 +0200

    Maybe we're getting somewhere

---
+++ Fire Flies/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed = 50;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        movementDirection.x *= speed;
+        movementDirection.z *= speed;
+        rb.MovePosition(transform.position + movementDirection * Time.deltaTime);
+        
+        if (movementDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), 0.15F);
+            transform.Translate(movementDirection * Time.deltaTime, Space.World);
+        }
+    }
+}
