e8af1bcca2ec6d666f2374e7d53f8cbfa6fb9bce
Manfredo Stagno d'Alcontres <manfredo@stagno.it>
2019-05-15 00:36:06 +0200

    Day 2: I knew this had potential

--- Fire Flies/Assets/Scripts/CharacterController.cs
+++ Fire Flies/Assets/Scripts/CharacterController.cs
@@ -7,13 +7,55 @@ public class CharacterController : MonoBehaviour
     Rigidbody rb;
 
     public float speed = 50;
+    public float decayTime = 25.0f;
+    public float deadTime = 5f;
+
+    private IEnumerator dieTimer;
+    private Light light;
+
+    private float fullLight;
+    private float lightMultiplier;
+    private float life;
+    private float lifeBar;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        light = GetComponentInChildren<Light>();
+        fullLight = light.intensity;
+        life = 0;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        Fade();
+
+        //if life regained stopcoroutine
+        if (lifeBar < 1 && dieTimer != null)
+        {
+            StopCoroutine(dieTimer);
+        }
+        //Run coroutine
+        if (lifeBar >= 1)
+        {
+            //return if coroutine active
+            if (dieTimer != null)
+                return;
+            //StartCoroutine
+            else
+                dieTimer = DieCoroutine();
+                StartCoroutine(dieTimer);
+        }
+    }
+
+    void Move()
     {
         Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         movementDirection.x *= speed;
@@ -25,5 +67,32 @@ public class CharacterController : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), 0.15F);
             transform.Translate(movementDirection * Time.deltaTime, Space.World);
         }
+    }
+
+    void Fade()
+    {
+        life += Time.deltaTime;
+
+        if (life > decayTime)
+            life = decayTime;
+        if (life < 0)
+            life = 0;
+        
+        lifeBar = life / decayTime; 
+        lightMultiplier = 1f - lifeBar;
+       
+        light.intensity = fullLight * lightMultiplier;
+    }
+
+    void Die()
+    {
+        //TODO: die method
+        Debug.Log("You Dead");
+    }
+
+    IEnumerator DieCoroutine()
+    {
+        yield return new WaitForSeconds(deadTime);
+        Die();
     }
 }
