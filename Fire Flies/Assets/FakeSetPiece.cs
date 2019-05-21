e2e05142e9db85da72071dcd795af893175fd6ce
Manfredo Stagno d'Alcontres <manfredo@stagno.it>
2019-05-21 16:19:35 +0200

    Build 0.2

---
+++ Fire Flies/Assets/FakeSetPiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeSetPiece : MonoBehaviour
+{
+
+    public GameObject father;
+    MeshRenderer mr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mr = GetComponentInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mr.enabled = false;
+        }
+    }
+}
